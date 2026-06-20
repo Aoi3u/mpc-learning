4ef5cdc27512c2b2b2fc4bf50c87a32d70300b2e
miu3u <miu.blue3@gmail.com>
2026-06-21 01:55:04 +0900

    add max_min

---
+++ python/max_min.py
@@ -0,0 +1,14 @@
+def max2(a, b):
+    if a < b:
+        return b
+    return a
+
+def min2(a, b):
+    if a < b:
+        return a
+    return b
+
+print(max2(1, 2))  # 2
+print(min2(1, 2))  # 1
+print(max2(5, 5))  # 5
+print(min2(5, 5))  # 5
